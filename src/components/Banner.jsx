759c5fc392ab33553719432286045e8347ef75cf
arcticcloudfox <27johnson279@gmail.com>
2024-09-03 16:55:09 -0500

    GA6 Final

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import "./styling.css"
+
+export default function Banner(){ //The <p> tag is just a placeholder message
+    return (
+        <div>
+        <header>Orbit Report</header>
+        <p>Click on the buttons to see the satellites in that orbit type</p>
+        </div>
+    )
+}
+
